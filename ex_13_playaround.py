ccfdd01f6596578790c1f1624ce34d536b48268c
Patrick OReilly <paddy1234@hotmail.com>
2020-10-14 07:13:26 +0100

    Ex17

--- ex_13_playaround.py
+++ ex_13_playaround.py
@@ -2,11 +2,12 @@
 from sys import argv
 
 def playground():
-    #Call the function in the command line followed by a number to fizzbuzz
+    #Call the function in the command line followed by a number to fizzbuzz. ie. 'python3 ex_13_playaround.py 3'
+
     """ playground """
 
     fb_num = int(argv[1])
-    fb_guess = input("Is the numbers fizzbuzz 'fizz', 'buzz', or nothing:\n")
+    fb_guess = input("Is the number's fizzbuzz: 'fizz', 'buzz', 'fizzbuzz' or nothing:\n->")
     if_fizz = 'fizz' if fb_num % 3 == 0 else ''
     if_buzz = 'buzz' if fb_num % 5 == 0 else ''
 
